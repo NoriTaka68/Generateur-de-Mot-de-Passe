7e4fb1d43f6f2a615b9fa5cdff6864e2bb81c92d
NoriTaka68 <said@evhr.net>
2022-07-24 10:30:15 +0200

    Add HomePage

--- generateur/views.py
+++ generateur/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return render(request,  'generateur\home.html', {'MDP': '123456'})
+
+def test(request):
+    return HttpResponse('Test')
