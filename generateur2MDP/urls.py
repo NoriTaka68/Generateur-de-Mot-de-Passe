5a1131cfc16f150eab550133d98204c9aef591d0
NoriTaka68 <said@evhr.net>
2022-07-25 02:29:41 +0200

    Creation de la page html

--- generateur2MDP/urls.py
+++ generateur2MDP/urls.py
@@ -20,5 +20,5 @@ from generateur import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home),
-    path('motdepasse',views.motdepasse),
+    path('passeWord',views.motdepasse, name='motdepasse'),
 ]
